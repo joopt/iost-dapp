d9492ba8b2d1bffd4b1859badb98f670a8e87472
joopt <ch0c0_b1rdy@hotmail.com>
2019-07-27 05:28:44 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,9 @@
-function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}
+function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}const rpc = new window.IOST.RPC(new IOST.HTTPProvider("https://api.iost.io"));
+const contractAddress = 'ContractGJK9J9oRe5Z2sgf8Rz3GvZQztLcfoZ8mwo2wurHJGFeX';
 
-const rpc = new window.IOST.RPC(new IOST.HTTPProvider("https://api.iost.io"));
-
-
-
-
-
-class HelloWorld extends React.Component {constructor(...args) {super(...args);_defineProperty(this, "state",
-    {
-      account: 'Please Login IWallet',
-      someone: '',
-      txHash: '',
-      result: '',
-      isLoading: false,
-      balancePlay: '',
-      balanceIost: '',
-      userBalance: '',
-      userBalancePlay: '',
-      playOwed: '' });_defineProperty(this, "handleChange",
-
+class HelloWorld extends React.Component {
+  constructor(props) {
+    super(props);_defineProperty(this, "handleChange",
 
     e => {
       this.setState({
@@ -26,21 +11,53 @@ class HelloWorld extends React.Component {constructor(...args) {super(...args);_
 
 
 
-    });_defineProperty(this, "hello",
+    });_defineProperty(this, "updateInfo",
+
+    account => {
+      rpc.blockchain.getBalance(account, "iost", 1).then(result => {
+        this.setState({
+          userBalance: result.balance.toFixed(2) });
+
+
+      });
+      rpc.blockchain.getBalance(account, "iplay", 1).then(result => {
+        this.setState({
+          userBalancePlay: result.balance.toFixed(2) });
+
+
+      });
+
+      rpc.blockchain.getBalance("ContractGJK9J9oRe5Z2sgf8Rz3GvZQztLcfoZ8mwo2wurHJGFeX", "iost", 1).then(balanceIost => {
+        this.setState({
+          balanceIost: balanceIost.balance.toFixed(2) });
+
+
+      });
+      rpc.blockchain.getBalance("ContractGJK9J9oRe5Z2sgf8Rz3GvZQztLcfoZ8mwo2wurHJGFeX", "iplay", 1).then(balancePlay => {
+        if (!balancePlay) return;
+        this.setState({
+          balancePlay: balancePlay.balance.toFixed(2) });
+
+      });
+      console.log('updated info');
+
+    });_defineProperty(this, "buyIplay",
 
     () => {
-      const contractAddress = 'ContractGJK9J9oRe5Z2sgf8Rz3GvZQztLcfoZ8mwo2wurHJGFeX';
-      const { someone } = this.state;
-      if (someone !== "") {
+
+      const {
+        userInput } =
+      this.state;
+      if (userInput !== "") {
         IWalletJS.enable().then(account => {
           if (account) {
             const iost = IWalletJS.newIOST(IOST);
-            const tx = iost.callABI(contractAddress, "buyPlay", [account, (someone * 0.02).toString()]);
+            const tx = iost.callABI(contractAddress, "buyPlay", [account, (userInput * 0.02).toString()]);
             tx.amount_limit = [{
               "token": "iost",
-              "value": (someone * 0.02).toString() }];
+              "value": (userInput * 0.02).toString() }];
 
-            console.log(tx.amount_limit);
+
 
 
             iost.signAndSend(tx).
@@ -53,40 +70,14 @@ class HelloWorld extends React.Component {constructor(...args) {super(...args);_
 
             }).
             on('success', result => {
-              console.log(result, someone, 'result');
+              console.log(result, userInput, 'result');
               this.setState({
                 isLoading: false,
-                result: `Success, you have received ${someone} iPLAY tokens` });
+                result: `Success, you have received ${userInput} iPLAY tokens` });
 
 
-              console.log('checkpoint 1');
-              rpc.blockchain.getBalance(account, "iost", 1).then(result => {
-                this.setState({
-                  userBalance: result.balance.toFixed(2) });
 
-                console.log('checkpoint 2');
-
-
-              });
-              rpc.blockchain.getBalance(account, "iplay", 1).then(result => {
-                this.setState({
-                  userBalancePlay: result.balance.toFixed(2) });
-
-
-              });
-
-              rpc.blockchain.getBalance("ContractGJK9J9oRe5Z2sgf8Rz3GvZQztLcfoZ8mwo2wurHJGFeX", "iost", 1).then(balanceIost => {
-                this.setState({
-                  balanceIost: balanceIost.balance.toFixed(2) });
-
-                console.log('checkpoint 2');
-              });
-              rpc.blockchain.getBalance("ContractGJK9J9oRe5Z2sgf8Rz3GvZQztLcfoZ8mwo2wurHJGFeX", "iplay", 1).then(balancePlay => {
-                if (!balancePlay) return;
-                this.setState({
-                  balancePlay: balancePlay.balance.toFixed(2) });
-
-              });
+              this.updateInfo(this.state.account);
 
             }).
             on('failed', failed => {
@@ -97,7 +88,7 @@ class HelloWorld extends React.Component {constructor(...args) {super(...args);_
 
             });
           } else {
-            console.log('not login');
+            console.log('Not logged in');
           }
 
         });
@@ -105,19 +96,21 @@ class HelloWorld extends React.Component {constructor(...args) {super(...args);_
         window.alert("Please input the amount you wish to buy");
       }
 
-    });_defineProperty(this, "transfer",
+    });_defineProperty(this, "sellIplay",
 
     () => {
-      const contractAddress = 'ContractGJK9J9oRe5Z2sgf8Rz3GvZQztLcfoZ8mwo2wurHJGFeX';
-      const { someone } = this.state;
-      if (someone !== "") {
+
+      const {
+        userInput } =
+      this.state;
+      if (userInput !== "") {
         IWalletJS.enable().then(account => {
           if (account) {
             const iost = IWalletJS.newIOST(IOST);
-            const tx = iost.callABI(contractAddress, "sellPlay", [account, someone]);
+            const tx = iost.callABI(contractAddress, "sellPlay", [account, userInput]);
             tx.amount_limit = [{
               "token": "iplay",
-              "value": someone.toString() }];
+              "value": userInput.toString() }];
 
             iost.signAndSend(tx).
             on('pending', pending => {
@@ -129,36 +122,12 @@ class HelloWorld extends React.Component {constructor(...args) {super(...args);_
 
             }).
             on('success', result => {
-              console.log(result, someone, 'result');
+              console.log(result, userInput, 'result');
               this.setState({
                 isLoading: false,
-                result: `Success! You have received ${(someone * 0.007).toFixed(4)} IOST` });
+                result: `Success! You have received ${(userInput * 0.007).toFixed(4)} IOST` });
 
-              rpc.blockchain.getBalance(account, "iost", 1).then(result => {
-                this.setState({
-                  userBalance: result.balance.toFixed(2) });
-
-
-              });
-              rpc.blockchain.getBalance(account, "iplay", 1).then(result => {
-                this.setState({
-                  userBalancePlay: result.balance.toFixed(2) });
-
-
-              });
-
-              rpc.blockchain.getBalance("ContractGJK9J9oRe5Z2sgf8Rz3GvZQztLcfoZ8mwo2wurHJGFeX", "iost", 1).then(balanceIost => {
-                this.setState({
-                  balanceIost: balanceIost.balance.toFixed(2) });
-
-
-              });
-              rpc.blockchain.getBalance("ContractGJK9J9oRe5Z2sgf8Rz3GvZQztLcfoZ8mwo2wurHJGFeX", "iplay", 1).then(balancePlay => {
-                if (!balancePlay) return;
-                this.setState({
-                  balancePlay: balancePlay.balance.toFixed(2) });
-
-              });
+              this.updateInfo(this.state.account);
             }).
             on('failed', failed => {
               console.log(failed, 'failed');
@@ -169,7 +138,7 @@ class HelloWorld extends React.Component {constructor(...args) {super(...args);_
 
             });
           } else {
-            console.log('not login');
+            console.log('Not logged in');
           }
         });
       } else {
@@ -177,58 +146,65 @@ class HelloWorld extends React.Component {constructor(...args) {super(...args);_
       }
 
 
-    });}componentDidMount() {// console.log(window.IWalletJS)
-    setTimeout(() => {IWalletJS.enable().then(account => {if (!account) return;this.setState({ account });rpc.blockchain.getBalance(account, "iost", 1).then(result => {this.setState({ userBalance: result.balance.toFixed(2) });});rpc.blockchain.getBalance(account, "iplay", 1).then(result => {this.setState({ userBalancePlay: result.balance.toFixed(2) });});rpc.blockchain.getBalance("ContractGJK9J9oRe5Z2sgf8Rz3GvZQztLcfoZ8mwo2wurHJGFeX", "iost", 1).then(balanceIost => {this.setState({ balanceIost: balanceIost.balance.toFixed(2) });});rpc.blockchain.getBalance("ContractGJK9J9oRe5Z2sgf8Rz3GvZQztLcfoZ8mwo2wurHJGFeX", "iplay", 1).then(balancePlay => {if (!balancePlay) return;this.setState({ balancePlay: balancePlay.balance.toFixed(2) });});});}, 500);}
+    });this.state = { account: 'Please Login IWallet', userInput: '', txHash: '', result: '', isLoading: false, balancePlay: '', balanceIost: '', userBalance: '', userBalancePlay: '' };this.updateInfo = this.updateInfo.bind(this);}componentDidMount() {setTimeout(() => {IWalletJS.enable().then(account => {if (!account) return;this.setState({ account: account });this.updateInfo(account);});}, 500);}
+
   render() {
-    const { txHash, result, isLoading, account, balancePlay, balanceIost, userBalance, userBalancePlay, someone } = this.state;
+    const {
+      txHash,
+      result,
+      isLoading,
+      account,
+      balancePlay,
+      balanceIost,
+      userBalance,
+      userBalancePlay,
+      userInput } =
+    this.state;
     return (
-      React.createElement("div", { className: "HelloWorld" },
+      React.createElement("div", null,
       React.createElement("div", { className: "content" },
-      React.createElement("header", { className: "HelloWorld__title" }, "Trade iPLAY"),
-      React.createElement("div", { className: "spacing" },
+      React.createElement("header", { className: "title" }, "Trade iPLAY"),
+      React.createElement("div", { className: "contractInfo" },
       React.createElement("h1", { className: "contractInfoTitle" }, "Contract Balance  "),
       React.createElement("h1", { className: "contractInfoText" }, " ", balanceIost, " IOST  "),
       React.createElement("h1", { className: "contractInfoText" }, " ", balancePlay, " iPLAY ")),
 
       React.createElement("input", {
-        className: "HelloWorld__input",
-        name: "someone",
+        className: "inputbox",
+        name: "userInput",
         autocomplete: "off",
         placeholder: "Enter amount of iPLAY tokens",
         onChange: this.handleChange }),
 
 
-      React.createElement("p", null, "Buy: ", (someone * 0.02).toFixed(3), " IOST | Sell: ", (someone * 0.007).toFixed(3), " IOST"),
-      React.createElement("div", { className: "spacing" },
-      React.createElement("h3", { className: "contractInfoTitle" }, "Account: ", account, "  "),
-      React.createElement("h3", { className: "contractInfoText" }, userBalance, " IOST"),
-      React.createElement("h3", { className: "contractInfoText" }, userBalancePlay, " iPLAY")),
+      React.createElement("p", null, "Buy: ", (userInput * 0.02).toFixed(3), " IOST | Sell: ", (userInput * 0.007).toFixed(3), " IOST"),
+      React.createElement("div", { className: "contractInfo" },
+      React.createElement("h1", { className: "contractInfoTitle" }, "Account: ", account, "  "),
+      React.createElement("h1", { className: "contractInfoText" }, userBalance, " IOST"),
+      React.createElement("h1", { className: "contractInfoText" }, userBalancePlay, " iPLAY")),
 
       React.createElement("div", { className: "btn-group" },
       React.createElement("button", {
-        className: "HelloWorld__helloButton",
-        onClick: this.hello }, "Buy iPLAY Tokens"),
+        className: "tradebutton",
+        onClick: this.buyIplay }, "Buy iPLAY Tokens"),
 
 
 
 
       React.createElement("button", {
-        className: "HelloWorld__helloButton",
-        onClick: this.transfer }, "Sell iPLAY Tokens")),
-
-
+        className: "tradebutton",
+        onClick: this.sellIplay }, "Sell iPLAY Tokens")),
 
 
       isLoading &&
 
-      React.createElement("div", { className: "HelloWorld__tx" },
-      React.createElement("p", null, " ", React.createElement("img", { className: "gif", src: "https://i.imgur.com/OVPxCO7.gif" }), "  Processing tx...")),
-
+      React.createElement("div", { className: "txtext" },
+      React.createElement("p", null, " ", React.createElement("img", { className: "pics", src: "https://i.imgur.com/OVPxCO7.gif" }), "  Processing tx...")),
 
 
 
       result &&
-      React.createElement("p", { className: "HelloWorld__tx" }, React.createElement("img", { className: "gif", src: "https://icon2.kisspng.com/20180320/tuw/kisspng-check-mark-computer-icons-clip-art-green-check-mark-2-icon-5ab1d1bfebf0f5.0376901715216030079664.jpg" }), " ", result)),
+      React.createElement("p", { className: "txtext" }, React.createElement("img", { className: "pics", src: "https://icon2.kisspng.com/20180320/tuw/kisspng-check-mark-computer-icons-clip-art-green-check-mark-2-icon-5ab1d1bfebf0f5.0376901715216030079664.jpg" }), " ", result)),
 
 
 
